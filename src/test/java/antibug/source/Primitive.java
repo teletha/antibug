31adb5ac96e47e55cf30b6283d0a6a788b5236af
Teletha <Teletha.T@gmail.com>
2014-08-04 13:21:35 +0900

    Support primitive.

---
+++ src/test/java/antibug/source/Primitive.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2014 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package antibug.source;
+
+/**
+ * @version 2014/08/04 13:17:36
+ */
+public class Primitive {
+
+    int INT = 1;
+
+    long LONG = 20L;
+
+    float FLOAT = 0.3F;
+
+    double DOUBLE = -0.5D;
+
+    byte BYTE = 2;
+
+    short SHORT = 12;
+
+    boolean BOOLEAN = true;
+}
