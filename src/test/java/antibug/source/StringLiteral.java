95ba5b1b6678c0c62bc6c6b60d9b0911b32e1bd9
Teletha <Teletha.T@gmail.com>
2014-08-05 11:13:55 +0900

    Support escaped character.

---
+++ src/test/java/antibug/source/StringLiteral.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2014 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package antibug.source;
+
+/**
+ * @version 2014/08/05 10:51:27
+ */
+public class StringLiteral {
+
+    String normal = "normal";
+
+    String nonAscii = "(´・ω・`)";
+
+    String escape = "\b\t\n\r\f\'\"\\";
+
+    // @TODO how to test
+    // String unicode = "\u3042\u3044\u3046\u3048\u304A";
+
+    char c = 'c';
+
+    char nonAsciiChar = '熊';
+
+    char backspace = '\b';
+
+    char tab = '\t';
+
+    char nobreak = '\n';
+
+    char carriageReturn = '\r';
+
+    char lineFeed = '\f';
+
+    char singleQuote = '\'';
+
+    char doubleQuote = '\"';
+
+    char escapeChar = '\\';
+}
