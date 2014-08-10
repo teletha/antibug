b4fba8eb3db33b9ddbd09c99ead93374ef4a4311
Teletha <Teletha.T@gmail.com>
2014-08-11 08:25:11 +0900

    Progress...

---
+++ src/test/java/antibug/source/link/Linker.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2014 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package antibug.source.link;
+
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+
+import kiss.Disposable;
+
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+
+import antibug.source.link.external.ExternalPackage;
+
+/**
+ * @version 2014/08/09 10:40:26
+ */
+public class Linker {
+
+    SamePackage samePackage;
+
+    ExternalPackage externalPackage;
+
+    String javaLang;
+
+    List javaUtil;
+
+    DocumentBuilder javax;
+
+    Element dom;
+
+    InputSource sax;
+
+    Disposable externalModule;
+
+    com.sun.tools.javac.util.List samaName;
+}
---
+++ src/test/java/antibug/source/link/SamePackage.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2014 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package antibug.source.link;
+
+
+/**
+ * @version 2014/08/09 10:39:12
+ */
+public class SamePackage {
+
+}
