bfaba12e32c8da5e95b4bd0d59f3ce76fb19b154
Teletha <Teletha.T@gmail.com>
2014-08-16 11:39:43 +0900

    Provide javadoc location info.

---
+++ src/main/java/antibug/javadoc/AntibugDocConfig.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2014 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package antibug.javadoc;
+
+/**
+ * @version 2014/08/16 9:30:52
+ */
+public class AntibugDocConfig {
+
+}
