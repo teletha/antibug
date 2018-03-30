9627fd6cddecbe82f0aff2f5842bf944f5550cc4
Teletha <Teletha.T@gmail.com>
2018-03-31 03:24:30 +0900

    Add power assert test.

---
+++ src/main/java/antibug/Test.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2018 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package antibug;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * @version 2018/03/31 3:23:22
+ */
+@Documented
+@Retention(RUNTIME)
+@Target(METHOD)
+@org.junit.jupiter.api.Test
+public @interface Test {
+    /**
+     * Switch power assert.
+     * 
+     * @return
+     */
+    boolean powerAssert() default true;
+}
