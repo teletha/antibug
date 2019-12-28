76312bc3bcceceff019ad3f0db85898f26e0301c
Teletha <Teletha.T@gmail.com>
2019-12-28 18:23:57 +0900

    Refactoring AntibugDoclet.

---
+++ src/test/java/antibug/doc/DocumentUser.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2019 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package antibug.doc;
+
+import java.io.IOException;
+
+public class DocumentUser {
+
+    public static void main(String[] args) throws IOException {
+        AntibugDoclet.Builder.sources("src/main/java").build();
+    }
+}
