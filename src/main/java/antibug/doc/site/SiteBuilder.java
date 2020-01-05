76f3a04ceae1b4a7e55f02921ae4def5c2107ee0
Teletha <Teletha.T@gmail.com>
2020-01-05 09:14:42 +0900

    Fix html serialization.

--- src/main/java/antibug/doc/site/SiteBuilder.java
+++ src/main/java/antibug/doc/site/SiteBuilder.java
@@ -111,7 +111,7 @@ public class SiteBuilder {
 
     private static final String[] characterType = {"figcaption", "figure", "a", "abbr", "b", "bdi", "bdo", "cite", "code", "data", "dfn",
             "em", "i", "kbd", "mark", "q", "rb", "rp", "rt", "rtc", "s", "samp", "strong", "sub", "sup", "time", "u", "var", "del", "ins",
-            "&script", "&nav"};
+            "&script", "&nav", "&article", "&aside"};
 
     /**
      * Build HTML file.
