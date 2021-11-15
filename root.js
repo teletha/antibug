e650f03b58147a212fe38f958e0843b3acfaa265
teletha <teletha@users.noreply.github.com>
2021-11-15 23:51:22 +0000

    deploy: d5e08b7f53fa842d6ae54f85dacc3024618566f8

---
+++ root.js
@@ -0,0 +1,188 @@
+const root = {
+	"docs": [],
+	"modules": [],
+	"packages": [
+		"antibug",
+		"antibug.bytecode",
+		"antibug.powerassert",
+		"antibug.profiler",
+		"org.junit.jupiter.engine.descriptor"
+	],
+	"types": [
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Agent",
+			"packageName": "antibug.bytecode",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Agent.Translator",
+			"packageName": "antibug.bytecode",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Benchmark",
+			"packageName": "antibug.profiler",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Bytecode",
+			"packageName": "antibug.bytecode",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Chronus",
+			"packageName": "antibug",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Chronus.Task",
+			"packageName": "antibug",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "CleanRoom",
+			"packageName": "antibug",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "CleanRoom.FileSystemDSL",
+			"packageName": "antibug",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "CommandLineUser",
+			"packageName": "antibug",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Constant",
+			"packageName": "antibug.bytecode",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Instruction",
+			"packageName": "antibug.bytecode",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "IntValue",
+			"packageName": "antibug.bytecode",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "LocalVariable",
+			"packageName": "antibug.bytecode",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "LocalVariablesSorter",
+			"packageName": "antibug.bytecode",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "MethodCall",
+			"packageName": "antibug.bytecode",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "PowerAssert",
+			"packageName": "antibug.powerassert",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "PowerAssertContext",
+			"packageName": "antibug.powerassert",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "PowerAssertOff",
+			"packageName": "antibug.powerassert",
+			"type": "Annotation"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "PowerAssertRenderer",
+			"packageName": "antibug.powerassert",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "PowerAssertionError",
+			"packageName": "antibug.powerassert",
+			"type": "Exception"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Profiler",
+			"packageName": "antibug.profiler",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Profiler.Result",
+			"packageName": "antibug.profiler",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "TestMethodTestDescriptor",
+			"packageName": "org.junit.jupiter.engine.descriptor",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "WebSocketServer",
+			"packageName": "antibug",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "WebSocketServer.WebSocketClient",
+			"packageName": "antibug",
+			"type": "Class"
+		}
+	]
+}
