163a49cd4cddea2d7ecb9f33942c4aa1cdcc843f
Teletha <Teletha.T@gmail.com>
2023-02-14 12:08:35 +0900

    revert

--- src/main/java/antibug/powerassert/PowerAssert.java
+++ src/main/java/antibug/powerassert/PowerAssert.java
@@ -9,13 +9,10 @@
  */
 package antibug.powerassert;
 
-import java.lang.reflect.Method;
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.function.Consumer;
 
-import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
-import org.junit.jupiter.api.extension.ExtensionContext.Store;
 import org.junit.jupiter.engine.execution.JupiterEngineExecutionContext;
 
 import antibug.bytecode.Agent;
@@ -37,9 +34,6 @@ public class PowerAssert {
      * @param errorExecutor
      */
     public static void capture(JupiterEngineExecutionContext context, Throwable error, Runnable testExecutor, Consumer<Throwable> errorExecutor) {
-        Method m = context.getExtensionContext().getRequiredTestMethod();
-        Store store = context.getExtensionContext().getStore(Namespace.GLOBAL);
-
         if (error instanceof PowerAssertionError) {
             PowerAssertionError e = (PowerAssertionError) error;
 
@@ -48,8 +42,6 @@ public class PowerAssert {
             } else {
                 errorExecutor.accept(error); // rethrow for unit test
             }
-        } else if (store.get(m) != null) {
-            errorExecutor.accept(error); // rethrow for unit test
         } else {
             try {
                 Throwable cause = error;
@@ -61,17 +53,16 @@ public class PowerAssert {
                         // !description.getTestClass()
                         // .isAnnotationPresent(PowerAssertOff.class)) {
 
-                        // store power assert process in extension context
-                        store.put(m, m);
+                        Class clazz = Class.forName(cause.getStackTrace()[0].getClassName());
 
-                        // translate assertion code only once
                         synchronized (PowerAssert.class) {
-                            Class clazz = Class.forName(cause.getStackTrace()[0].getClassName());
-                            if (translated.add(clazz.getName())) agent.transform(clazz);
+                            // translate assertion code only once
+                            if (translated.add(clazz.getName())) {
+                                agent.transform(clazz);
+                                testExecutor.run();
+                                return;
+                            }
                         }
-
-                        testExecutor.run();
-                        return;
                     }
                     cause = cause.getCause();
                 }
